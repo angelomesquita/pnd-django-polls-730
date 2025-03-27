43055e545c094417864545884b7e844e7bc8d6dc
Angelo Mesquita <angeloamesquita@gmail.com>
2025-03-27 11:03:48 -0300

    parte 3: usando o atalho render

--- polls/views.py
+++ polls/views.py
@@ -7,9 +7,8 @@ from .models import Question
 # Question “index” page – displays the latest few questions.
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = { "latest_question_list": latest_question_list }
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 # Question “detail” page – displays a question text, with no results but with a form to vote.
 def detail(request, question_id):
