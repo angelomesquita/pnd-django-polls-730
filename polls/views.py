7b83434a911ff6388dfd723765279e7b837e7cc3
Angelo Mesquita <angeloamesquita@gmail.com>
2025-03-27 10:39:25 -0300

    parte 3: escrevendo mais views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# View index /
+# Question “index” page – displays the latest few questions.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+# Question “detail” page – displays a question text, with no results but with a form to vote.
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+# Question “results” page – displays results for a particular question.
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+# Vote action – handles voting for a particular choice in a particular question.
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
