1bf482c3ff70305f685f9daf81fb0426e2e4ab70
Angelo Mesquita <angeloamesquita@gmail.com>
2025-04-10 09:39:07 -0300

    parte 4: crie um formulário simples (versão real do vote)

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
 
-from .models import Question
+from .models import Choice, Question
 
 
 # Question “index” page – displays the latest few questions.
@@ -25,4 +25,23 @@ def results(request, question_id):
 
 # Vote action – handles voting for a particular choice in a particular question.
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
